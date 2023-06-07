de2710dd6c91e8b018f0776e745af9a2dfc041b0
dvangeest <302942745@student.rocmondriaan.nl>
2023-06-07 12:49:53 +0200

    progress to creating Project Data Objects

---
+++ js/main.js
@@ -0,0 +1,64 @@
+const modalProjectData = [
+    {
+        title: 'Four in a Row',
+        description: `
+            I created a challenging version of Tic Tac Toe called "Four in a Row" using HTML, CSS, and JavaScript.
+            <br><br>
+            Originally designed for Player vs. Player, I added an AI opponent for a higher grade. The computer's moves were randomized, making the game unpredictable and engaging.
+            <br><br>
+            The interface was user-friendly, displaying the current player's turn, name and the game result. It was an opportunity to showcase my skills in web development and strategic thinking.
+        `,
+        img: 'img/four-in-a-row.png',
+        repo: 'https://github.com/ROCMondriaanTIN/sd22-project-p02-boter-kaas-en-eieren-dvangeest.git',
+        demo: '',
+    },
+    {
+        title: 'Streaming Service',
+        description: `
+        For my solo school project, I created a streaming service website within a week. The website allowed users to create an account, log in, and browse a library of shows for streaming. Due to limitations, the actual streaming functionality was simulated using placeholder videos instead of sourcing real shows.
+        <br><br>
+        I developed the project using HTML, CSS, and JavaScript, showcasing my proficiency in these web technologies. The user interface was carefully designed to provide a visually appealing and intuitive experience. I focused on optimizing the navigation flow and ensuring a seamless user journey throughout the website.
+        <br><br>
+        To handle user accounts, I implemented a login system using JavaScript, where user information was securely stored in the browser's local storage. As a result, the login functionality did not send data to a server but provided a simulated user authentication experience.
+        <br><br>
+        Given the time constraints, I prioritized essential features and functionalities to deliver a functional website within the given timeframe. The project demonstrated my ability to work independently and create a streaming service website, complete with user account functionality.
+        <br><br>
+        Overall, this solo project highlighted my proficiency in HTML, CSS, and JavaScript, as well as my ability to develop a streaming service website with user account management. While actual streaming was not available, the project provided users with an immersive experience using placeholder videos and showcased my skills in web development and rapid project execution.
+        `,
+        img: 'img/steaming-service.jpeg',
+        repo: 'https://github.com/ROCMondriaanTIN/sd22-themaweek-2-t1k-dvangeest.git',
+        demo: '',
+    },
+    {
+        title: 'Attack on Titan: Evolution (Roblox)',
+        description: `
+        "Attack on Titan: Evolution" was a highly anticipated Roblox game based on the popular anime series "Attack on Titan" (AoT). Developed by a team of approximately six developers, including myself as an assistant programmer and responsible for the User Interface (UI), the project spanned over a period of two years.
+        <br><br>
+        Our goal was to create an immersive gaming experience that captured the essence and thrilling gameplay of the Attack on Titan universe. With meticulous attention to detail, we crafted a world where players could assume the role of Scouts, don their Omni-Directional Mobility Gear, and engage in high-flying battles against the towering Titans.
+        <br><br>
+        Throughout the development process, my primary focus was on implementing intuitive and visually appealing UI elements. I worked closely with the team to ensure a seamless integration of UI components, including menus, health bars, map displays, and player interactions. By designing a user-friendly interface, we aimed to enhance player immersion and facilitate seamless gameplay.
+        <br><br>
+        After two years of dedicated work, "Attack on Titan: Evolution" was successfully released to the Roblox platform. The game garnered positive feedback from the community, drawing in a substantial player base and fostering an active community of fans.
+        <br><br>
+        However, as time went on, our team's interest and priorities shifted, prompting us to make the decision to sell the game to "Boss Studio." This allowed us to pursue new ventures and projects that aligned more closely with our evolving interests and aspirations.
+        <br><br>
+        Overall, the development of "Attack on Titan: Evolution" was an exhilarating journey, marked by collaboration, creativity, and a deep passion for the Attack on Titan anime. As an assistant programmer and UI specialist, I played a crucial role in creating an immersive user experience. The success and subsequent sale of the game demonstrated our team's dedication, talent, and adaptability in the dynamic world of Roblox game development.
+        `,
+    },
+    {
+        title: 'Escape Room',
+        description: `
+        In a group project with two classmates, we developed a captivating 2D Escape Room web game using HTML, CSS, and JavaScript. 
+        <br><br>
+        Inspired by the anime "Death Parade," the game featured a compelling storyline and immersive visuals. 
+        <br><br>
+        Players had to solve puzzles and riddles to progress through the narrative and escape the room. Our collaboration showcased our skills in web development and storytelling, creating an engaging experience for players.
+        `,
+        img: 'img/escape-room.jpeg',
+        repo: 'https://github.com/dvangeest/escape-room.git',
+        demo: '',
+    },
+    {
+
+    },
+]
