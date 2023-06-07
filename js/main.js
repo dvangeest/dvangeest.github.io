bba21505d851c28c19770ad85bcf4b9f29497e25
dvangeest <302942745@student.rocmondriaan.nl>
2023-06-07 15:15:15 +0200

    modal Project data completed

--- js/main.js
+++ js/main.js
@@ -44,6 +44,22 @@ const modalProjectData = [
         <br><br>
         Overall, the development of "Attack on Titan: Evolution" was an exhilarating journey, marked by collaboration, creativity, and a deep passion for the Attack on Titan anime. As an assistant programmer and UI specialist, I played a crucial role in creating an immersive user experience. The success and subsequent sale of the game demonstrated our team's dedication, talent, and adaptability in the dynamic world of Roblox game development.
         `,
+        img: 'img/aote-banner.png',
+        demo: 'https://www.roblox.com/games/7127407851/',
+    },
+    {
+        title: 'Uno (Roblox)',
+        description: `
+        Following the success of "Attack on Titan: Evolution" on Roblox, I embarked on creating an exciting and immersive Roblox Uno game. This 3D adaptation of the classic card game aimed to provide a fun and engaging multiplayer experience for up to 16 players.
+        <br><br>
+        The game featured the traditional Uno gameplay, where players take turns matching colors or numbers to get rid of their cards. I incorporated interactive 3D elements to enhance the visual appeal and bring the game to life within the Roblox platform.
+        <br><br>
+        In addition to the traditional Uno mode, I introduced custom game modes to add variety and excitement. One such mode was "Wild West," a unique twist on Uno with a deck full of custom wild cards. This mode added a new layer of strategy and unpredictability, giving players a fresh and thrilling Uno experience.
+        <br><br>
+        Throughout the development process, I focused on ensuring smooth gameplay and intuitive controls. Players could easily navigate the 3D environment, interact with cards, and enjoy a seamless multiplayer experience with friends or other Roblox users.
+        `,
+        img: 'img/placeholder.jpeg',
+        demo: 'https://www.roblox.com/games/13244479228/',
     },
     {
         title: 'Escape Room',
@@ -59,6 +75,27 @@ const modalProjectData = [
         demo: '',
     },
     {
-
+        title: 'Old Portfolio',
+        description: `
+            I created my first portfolio website using HTML, CSS, and JavaScript. The website showcased my skills in web development and design, providing a visually appealing and intuitive user experience.
+            <br><br>
+            The website featured a responsive design, allowing users to access the website on any device.
+        `,
+        img: 'img/old-portfolio.png',
+        repo: 'https://github.com/ROCMondriaanTIN/sd22-design-p2-portfolio-dvangeest.git',
+        demo: '',
+    },
+    {
+        title: 'Higher Lower',
+        description: `
+        As my first school project, I developed a web-based game called "Higher Lower" that aimed to provide a simple and entertaining experience. The game was designed using HTML, CSS, and JavaScript, with my limited knowledge in these technologies at the time.
+        <br><br>
+        The objective of the game was straightforward: players started with a set amount of credits that they could bet. The computer would roll their dice, and then the player had to predict whether their own dice roll would be higher or lower. Alternatively, players had the option to draw for a potentially higher reward, but with increased risk.
+        <br><br>
+        As my first project, "Higher Lower" was a stepping stone in my journey as a developer. It provided valuable hands-on experience in HTML, CSS, and JavaScript while introducing me to the process of designing and implementing a web-based game. While simple in nature, the project allowed me to explore the fundamentals of web development and gain a foundation for future projects.
+        `,
+        img: 'img/higher-lower.png',
+        repo: 'https://github.com/dvangeest/higher-lower.git',
+        demo: '',
     },
 ]
